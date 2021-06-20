dccf08db6c77d7a8ab7e473b6d0faca13aaba612
prashantpandey1 <prashantpandey2709@gmail.com>
2021-06-20 23:46:54 +0530

    post req done

--- app/controllers/ajax_practice_controller.rb
+++ app/controllers/ajax_practice_controller.rb
@@ -2,6 +2,16 @@ class AjaxPracticeController < ApplicationController
     def ajax
     end
 
-      def ajax_call
+    def ajax_call
+    end
+
+    def virtualdata
+
+    end
+
+    def postlead
+        respond_to do |format|
+         format.json { render json: { success: true, name: "Rohit", drink: "Orange Juice"} }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'ajax_practice#ajax'
    get 'ajax' => 'ajax_practice#ajax'
-  get 'virtualdata' => 'ajax_practice#virtualdata'
-  get 'ajaxcall' => 'ajax_practice#ajax_call'
+   get 'ajaxcall' => 'ajax_practice#ajax_call'
+   get 'virtualdata' => 'ajax_practice'
+   post 'virtualdata' => 'ajax_practice#virtualdata'
+   post 'postlead' => 'ajax_practice#postlead'
 end
